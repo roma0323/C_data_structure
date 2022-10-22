8913714748fd4156a7e9a27a6a159169a39b2015
roma0323 <r911223oger@gmail.com>
2022-10-22 16:34:48 +0800

    basic input and nest struct

--- 2st_HW/queue_HW.c
+++ 2st_HW/queue_HW.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #define MAXQUEUE 6
 struct queue_mode{
+
+    struct guest_mode{
+        char number[10];
+        int amount ;
+    };
+
     int item[MAXQUEUE];
     int front ,rear;
+
+    struct guest_mode guest[MAXQUEUE]; 
 }queue,queue2;
-struct guest_mode{
-    char number[100];
-    int amount ;
-}guest;
+
 void ADDQUEUE(int element_item){                    //檢查重複代號
     if(queue.front==(queue.rear+1)%MAXQUEUE){
         printf("full\n");                           //最後要拿掉
@@ -34,24 +39,60 @@ void DELETEQUEUE(){
 
 }
 int main(){
+    
+    int input_mode=0;
+    while(input_mode!=-1){
+        scanf("%d",&input_mode);
+
+    switch(input_mode){
+        case 1:
+            printf("mode1\n");
+            break;
+        case 2:
+            printf("mode2\n");
+            break;
+        case 3:
+            printf("mode3\n");
+            break;
+        case 4:
+            printf("mode4\n");
+            break;
+        case -1:
+            break;
+        default:
+            printf("error");
+    }
+    }
+    
+        
+
+    queue.guest[1].amount=255;
+    printf("%d\n",queue.guest[1].amount);
+    strcpy( queue.guest[1].number, "5" );
+    //queue.guest[1].number="5";
+    printf("%s\n",queue.guest[1].number);
+    //printf("%d\n",queue.guest[1].number);
+    //printf("%cn\n",queue.guest[1].number);
+
+
     queue.front=0;
     queue.rear=0;
-    ADDQUEUE(11);
-    ADDQUEUE(222);
-    ADDQUEUE(333);
-    ADDQUEUE(4);
-    ADDQUEUE(5);
+    // ADDQUEUE(11);
+    // ADDQUEUE(222);
+    // ADDQUEUE(333);
+    // ADDQUEUE(4);
+    // ADDQUEUE(5);
     
 
-    ADDQUEUE(6);
-    DELETEQUEUE();
-    ADDQUEUE(6);
-    DELETEQUEUE();
-    DELETEQUEUE();
-    DELETEQUEUE();
-    DELETEQUEUE();
-    DELETEQUEUE();
-    DELETEQUEUE();
+    // ADDQUEUE(6);
+    // DELETEQUEUE();
+    // ADDQUEUE(6);
+    // DELETEQUEUE();
+    // DELETEQUEUE();
+    // DELETEQUEUE();
+    // DELETEQUEUE();
+    // DELETEQUEUE();
+    // DELETEQUEUE();
 
     
 
